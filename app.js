84889d31e2ba6cf700c357b0962fea53da5fee2f
Azhar Abbass <azharabbass786.com@gmail.com>
2024-08-18 16:01:17 +0500

    Created page for single location.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 const MONGO_URL = "mongodb://127.0.0.1/staymingle";
 
@@ -28,6 +29,16 @@ app.get("/listings", async (req, res) =>{
     const allListings = await Listing.find();
     res.render("listings/index.ejs", {allListings});
 });
+
+app.get("/listings/:id", async (req, res) =>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/create.ejs");
+})
 
 // app.get("/testListing", async (req,res) =>{
 //     let sampleListing = new Listing ({
