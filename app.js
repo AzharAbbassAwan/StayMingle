2f1ede99550662dc160b07599c2586814ee1af39
Azhar Abbass <azharabbass786.com@gmail.com>
2024-08-18 15:06:51 +0500

    created fake data for database init.

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1/staymingle";
+
+main().then(() =>{
+    console.log("Connected to DB");
+})
+.catch((err) =>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) =>{
+    res.send("Hi, I am root");
+});
+
+app.get("/testListing", async (req,res) =>{
+    let sampleListing = new Listing ({
+        title: "Safary Villas 2",
+        description: "Hilly areas",
+        price: 1120,
+        loacation: "Baheria town phase 7",
+        country : "Pakistan"
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfully listening is created!");
+})
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+})
---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+title:{
+    type: String,
+    required: true,
+},
+description:{
+    type: String,
+},
+image:{
+    type: String,
+    default: "https://media.istockphoto.com/id/1162069138/photo/green-waterfall.jpg?s=1024x1024&w=is&k=20&c=hDH3gltdRzPO7Y5SpFusws_bKzegETn2wc_A4UYwn8s=",
+    set:(v) => v === "" ? "https://media.istockphoto.com/id/1162069138/photo/green-waterfall.jpg?s=1024x1024&w=is&k=20&c=hDH3gltdRzPO7Y5SpFusws_bKzegETn2wc_A4UYwn8s=" : v,
+},
+price: Number,
+location: String,
+country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
