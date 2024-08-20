3fe589fda86df538c977af7d3705a65fec9f5a2c
azharabbass123 <azharabbass786.com@gmail.com>
2024-08-20 11:21:36 -0700

    optimize validation.

--- models/listing.js
+++ models/listing.js
@@ -9,13 +9,21 @@ title:{
 description:{
     type: String,
 },
-image:{
+image: {
     type: Object,
-    default: "https://media.istockphoto.com/id/1162069138/photo/green-waterfall.jpg?s=1024x1024&w=is&k=20&c=hDH3gltdRzPO7Y5SpFusws_bKzegETn2wc_A4UYwn8s=",
-    set:(v) =>
-         v === "" 
-            ? "https://media.istockphoto.com/id/1162069138/photo/green-waterfall.jpg?s=1024x1024&w=is&k=20&c=hDH3gltdRzPO7Y5SpFusws_bKzegETn2wc_A4UYwn8s="
-             : v,
+    default: {
+        filename: "listingimage",
+        url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    set: (v) => {
+        if (v === "") {
+            return {
+                filename: "listingimage",
+                url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            };
+        }
+        return v;
+    },
 },
 price: Number,
 location: String,
