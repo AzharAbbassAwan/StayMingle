7f7fc9ec3e755d095b76f93a81dd5f9d10c5ed91
Azhar Abbass <azharabbass786.com@gmail.com>
2024-08-31 04:15:09 +0500

    modifying schema for listings.

--- models/listing.js
+++ models/listing.js
@@ -12,19 +12,8 @@ description:{
 },
 image: {
     type: Object,
-    default: {
-        filename: "listingimage",
-        url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    },
-    set: (v) => {
-        if (v === "" || v === null) {
-            return {
-                filename: "listingimage",
-                url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-            };
-        }
-        return v;
-    },
+    url: String,
+    filename: String,
 },
 price: Number,
 location: String,
