d989a7494314b86e1f7fb1b0a65ff3777d02f065
Azhar Abbass <azharabbass786.com@gmail.com>
2024-08-31 04:21:12 +0500

    storing images on cloudinary

--- controllers/listings.js
+++ controllers/listings.js
@@ -28,6 +28,7 @@ module.exports.creat = async(req, res, next) =>{
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();
     req.flash("success", "New Listing Creatd");
     res.redirect("/listings");
