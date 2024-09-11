f8dec0388a25add4cd559dcc4517e8bf0e3711f4
azharabbass123 <azharabbass786.com@gmail.com>
2024-09-12 02:11:28 +0500

    modifying styles for responsive design.

--- public/js/map.js
+++ public/js/map.js
@@ -3,5 +3,5 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 L.marker([ lattitude, longitude ]).addTo(map)
-    .bindPopup(`${locationName} <br> exact location will be provided after booking.`)
+    .bindPopup(`${locationName} <br> exact location will be provided <br> after booking.`)
     .openPopup();
